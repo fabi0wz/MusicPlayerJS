40d3f6088f95bbaf4512f5ffbffd7abbb9436e7b
Fábio <117021038+fabi0wz@users.noreply.github.com>
2023-05-02 23:32:12 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const audioPlayer = document.getElementById('audio-player'); // Music Player
+const songName = document.getElementById('musictitle'); // Music Title
+const artist = document.getElementById('artist'); // Artist Name
+
