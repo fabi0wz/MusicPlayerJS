b8ed7a80f41fd690ff0ef376f5d9e528043fb76e
Fábio <fabiomiguel3.10@gmail.com>
2023-05-06 16:38:31 +0100

    updated

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-const audioPlayer = document.getElementById('audio-player'); // Music Player
-const songName = document.getElementById('musictitle'); // Music Title
-const artist = document.getElementById('artist'); // Artist Name
+const audioPlayer = document.querySelector('#audio-player'); // Music Player
+const songName = document.querySelector('#musictitle'); // Music Title
+const artist = document.querySelector('#artist'); // Artist Name
+
+
+const songCover = document.querySelector('#imgAudio');
+const audioControls = document.querySelector('.audio-volume-controls');
+const muteButton = audioControls.querySelector('#btnSoundOff');
+const unmuteButton = audioControls.querySelector('#btnSoundOn');
+const volumeBar = document.querySelector('#soundVolumeContainer');
+const volumeBarFill = volumeBar.querySelector('#soundVolumeLevel');
+
+const musicProgress = document.querySelector('#trackProgressContainer');
+
+
 
