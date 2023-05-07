edbb3995c6966a1d87a53d97ba1ebab5dac4fd53
Fábio <fabiomiguel3.10@gmail.com>
2023-05-07 03:19:19 +0100

    i fucked up lol

---
+++ main.js
@@ -0,0 +1,16 @@
+const audioPlayer = document.querySelector('#audio-player'); // Music Player
+const songName = document.querySelector('#musictitle'); // Music Title
+const artist = document.querySelector('#artist'); // Artist Name
+
+
+const songCover = document.querySelector('#imgAudio');
+const audioControls = document.querySelector('.audio-volume-controls');
+const muteButton = audioControls.querySelector('#btnSoundOff');
+const unmuteButton = audioControls.querySelector('#btnSoundOn');
+const volumeBar = document.querySelector('#soundVolumeContainer');
+const volumeBarFill = volumeBar.querySelector('#soundVolumeLevel');
+
+const musicProgress = document.querySelector('#trackProgressContainer');
+
+
+
